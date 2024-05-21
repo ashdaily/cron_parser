4cbbbe5e888782d161034e9cb8691e6c43869c80
ashdaily <ashtokyo31@gmail.com>
2024-05-22 00:21:23 +0100

    support year field as well

--- src/cron_parser.py
+++ src/cron_parser.py
@@ -12,15 +12,27 @@ class CronParser:
             raise ValueError(error_msg)
 
         # TODO: more validation needed ?
+        num_of_field = len(fields)
 
-        self.fields = {
-            "minute": CronFieldFactory.create_field("minute", fields[0]),
-            "hour": CronFieldFactory.create_field("hour", fields[1]),
-            "day of month": CronFieldFactory.create_field("day of month", fields[2]),
-            "month": CronFieldFactory.create_field("month", fields[3]),
-            "day of week": CronFieldFactory.create_field("day of week", fields[4]),
-            "command": CronFieldFactory.create_field("command", ' '.join(fields[5:]))
-        }
+        if num_of_field == 6:
+            self.fields = {
+                "minute": CronFieldFactory.create_field("minute", fields[0]),
+                "hour": CronFieldFactory.create_field("hour", fields[1]),
+                "day of month": CronFieldFactory.create_field("day of month", fields[2]),
+                "month": CronFieldFactory.create_field("month", fields[3]),
+                "day of week": CronFieldFactory.create_field("day of week", fields[4]),
+                "command": CronFieldFactory.create_field("command", ' '.join(fields[5:]))
+            }
+        else:
+            self.fields = {
+                "minute": CronFieldFactory.create_field("minute", fields[0]),
+                "hour": CronFieldFactory.create_field("hour", fields[1]),
+                "day of month": CronFieldFactory.create_field("day of month", fields[2]),
+                "month": CronFieldFactory.create_field("month", fields[3]),
+                "day of week": CronFieldFactory.create_field("day of week", fields[4]),
+                "year": CronFieldFactory.create_field("year", fields[5]),
+                "command": CronFieldFactory.create_field("command", ' '.join(fields[6:]))
+            }
 
     def parse(self):
         parsed_fields = {name: field.parse() for name, field in self.fields.items()}
