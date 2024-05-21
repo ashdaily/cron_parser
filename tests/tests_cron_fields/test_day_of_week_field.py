ce074dd5eb717de20bb0751c2907310d499ac0d8
ashdaily <ashtokyo31@gmail.com>
2024-05-22 00:22:38 +0100

    update tests

--- tests/tests_cron_fields/test_day_of_week_field.py
+++ tests/tests_cron_fields/test_day_of_week_field.py
@@ -11,6 +11,7 @@ class TestDayOfWeekField(unittest.TestCase):
         ("1-5", [1, 2, 3, 4, 5]),
         ("2-6/2", [2, 4, 6]),
         ("1-2/1,3-7/2", [1, 2, 3, 5, 7]),
+        ("mon-fri", [2, 3, 4, 5, 6]),
     ])
     def test_day_of_week_field(self, cron_string, expected_output):
         field = DayOfWeekField(cron_string)
